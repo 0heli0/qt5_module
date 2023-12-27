f78e0d18ecaff94fc0d1b42609ded72ae823bd04
csrd2022/heli <heli@csrd.cn>
2023-12-27 16:08:48 +0800

    5.2 complete

--- chap05/sample_5_2/widget.cpp
+++ chap05/sample_5_2/widget.cpp
@@ -1,5 +1,7 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDebug>
+#include <QString>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -7,13 +9,72 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
     theModel=new QStringListModel(this);
-    QStringList strList;
     strList<<"北京"<<"上海"<<"成都"<<"深圳"<<"杭州"<<"广州";
     theModel->setStringList(strList);
     ui->listView->setModel(theModel); //为listView设置数据模型
+    ui->listView->setEditTriggers(QAbstractItemView::DoubleClicked|QAbstractItemView::SelectedClicked);
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::on_btnTextImport_clicked()
+{
+    ui->plainTextEdit->clear();
+    QStringList strList = theModel->stringList();
+    foreach (auto str, strList) {
+     ui->plainTextEdit->appendPlainText(str);
+    }
+}
+
+void Widget::on_btnTextClear_clicked()
+{
+    ui->plainTextEdit->clear();
+}
+
+void Widget::on_btnListClear_clicked()
+{
+    strList.clear();
+    theModel->removeRows(0,theModel->rowCount());
+}
+
+void Widget::on_btnIniList_clicked()
+{
+    strList.clear();
+    strList<<"北京"<<"上海"<<"成都"<<"深圳"<<"杭州"<<"广州";
+    theModel->setStringList(strList);
+}
+
+void Widget::on_btnListAppend_clicked()
+{
+    strList.append("newItem");
+    theModel->setStringList(strList);
+    QModelIndex index = theModel->index(strList.size()-1);
+    ui->listView->setCurrentIndex(index);
+}
+
+void Widget::on_btnListInsert_clicked()
+{
+    int rowCount = ui->listView->currentIndex().row();
+    QModelIndex index = theModel->index(rowCount);
+    qDebug("int: %d",rowCount);
+    strList.insert(rowCount,"newItem");
+    theModel->setStringList(strList);
+    ui->listView->setCurrentIndex(index);
+
+}
+
+void Widget::on_btnListDelete_clicked()
+{
+    int rowCount = ui->listView->currentIndex().row();
+    qDebug("int: %d",rowCount);
+    strList.removeAt(rowCount);
+    theModel->setStringList(strList);
+}
+
+void Widget::on_listView_clicked(const QModelIndex &index)
+{
+    ui->label->setText(QString::asprintf("行: %d, 列: %d",index.row(),index.column()));
 }
--- chap05/sample_5_2/widget.h
+++ chap05/sample_5_2/widget.h
@@ -13,10 +13,28 @@ class Widget : public QWidget
     Q_OBJECT
 private:
     QStringListModel   *theModel; //数据模型
+    QStringList strList;
 
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
+
+private slots:
+    void on_btnTextImport_clicked();
+
+    void on_btnTextClear_clicked();
+
+    void on_btnListClear_clicked();
+
+    void on_btnIniList_clicked();
+
+    void on_btnListAppend_clicked();
+
+    void on_btnListInsert_clicked();
+
+    void on_btnListDelete_clicked();
+
+    void on_listView_clicked(const QModelIndex &index);
 
 private:
     Ui::Widget *ui;
