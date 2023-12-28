a2c16c1b58644fccab6893ff1594fd173f272c1b
csrd2022/heli <heli@csrd.cn>
2023-12-28 08:42:35 +0800

    5.3 start

---
+++ chap05/sample5_3/mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //创建状态栏组件
+    LabCurFile = new QLabel("当前文件：",this);
+    LabCurFile->setMinimumWidth(200);
+
+    LabCellPos = new QLabel("当前单元格：",this);
+    LabCellPos->setMinimumWidth(180);
+//    LabCellPos->setAlignment(Qt::AlignHCenter);
+
+    LabCellText = new QLabel("单元格内容：",this);
+    LabCellText->setMinimumWidth(150);
+
+    theModel = new QStandardItemModel(10,FixedColumnCount,this); //数据模型
+    theSelection = new QItemSelectionModel(theModel);// Item选择模型
+    ui->tableView->setModel(theModel);
+    ui->tableView->setSelectionModel(theSelection);
+    ui->statusBar->addWidget(LabCurFile);
+    ui->statusBar->addWidget(LabCellPos);
+    ui->statusBar->addWidget(LabCellText);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ chap05/sample5_3/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include    <QLabel>
+#include    <QStandardItemModel>
+#include    <QItemSelectionModel>
+
+#define     FixedColumnCount    6       //文件固定6列
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    //用于状态栏的信息显示
+    QLabel  *LabCurFile;  //当前文件
+    QLabel  *LabCellPos;    //当前单元格行列号
+    QLabel  *LabCellText;   //当前单元格内容
+
+    QStandardItemModel  *theModel;//数据模型
+    QItemSelectionModel *theSelection;//Item选择模型
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
